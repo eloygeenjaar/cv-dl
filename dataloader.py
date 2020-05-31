3c8efeaca47dc4e90eabd6d30b0a7a012c535488
Eloy Geenjaar <eloyg@live.nl>
2020-05-31 12:50:54 +0200

    Updated version, still needs a little more documentation '

--- dataloader.py
+++ dataloader.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 from typing import Tuple, List
-from PIL import Image
 import numpy as np
-from time import perf_counter
 import tensorflow as tf
 import keras
-
+import cv2
+from tqdm import tqdm
 
 class ChestXRayDataLoader(keras.utils.Sequence):
     """
@@ -18,6 +17,7 @@ class ChestXRayDataLoader(keras.utils.Sequence):
                  window_size: Tuple[int, int],
                  n_channels: int,
                  batch_size: int,
+                 n_patches_per_file: int,
                  shuffle: bool = True,
                  extension: str = '.jpeg'):
 
@@ -25,29 +25,23 @@ class ChestXRayDataLoader(keras.utils.Sequence):
         self.window_size: Tuple[int, int] = window_size
         self.n_channels: int = n_channels
         self.batch_size: int = batch_size
+        self.n_patches_per_file: int = n_patches_per_file
         self.shuffle: bool = shuffle
         self.extension: str = extension
         self.indexes: np.ndarray = np.array([])
 
-        file_gen_healthy = self.path.glob(f'healthy/*{self.extension}')
-        healthy_files = [f for f in file_gen_healthy if f.is_file()]
-        print(f'{len(healthy_files)} images found in {self.path / "healthy"}')
-        file_gen_pneumonia = self.path.glob(f'pneumonia/*{self.extension}')
-        pneumonia_files = [f for f in file_gen_pneumonia if f.is_file()]
-        print(f'{len(pneumonia_files)} images found in {self.path / "pneumonia"}')
-        files = healthy_files + pneumonia_files
-        temp_batch = []
-        for f in files:
-            img = Image.open(f)
-            patches = self.__sliding_window_patches(np.array(img))
-            temp_batch.append(patches)
-
-        self.total_batch = np.vstack(temp_batch)
-        self.total_batch = np.divide(self.total_batch, 255.0).astype(np.float32)
+        self.file_gen_healthy = self.path.glob(f'NORMAL/*{self.extension}')
+        healthy_files = [f for f in self.file_gen_healthy if f.is_file()]
+        print(f'{len(healthy_files)} images found in {self.path / "NORMAL"}')
+        self.file_gen_pneumonia = self.path.glob(f'PNEUMONIA/*{self.extension}')
+        pneumonia_files = [f for f in self.file_gen_pneumonia if f.is_file()]
+        print(f'{len(pneumonia_files)} images found in {self.path / "PNEUMONIA"}')
+        self.files = healthy_files + pneumonia_files
+        self.targets = np.array([0 if f in healthy_files else 1 for f in self.files])
         self.on_epoch_end()
 
     def __len__(self) -> int:
-        return int(np.floor(self.total_batch.shape[0] / self.batch_size))
+        return int(np.floor(len(self.files) / self.batch_size))
 
     def __getitem__(self, item) -> Tuple[np.ndarray, np.ndarray]:
 
@@ -55,25 +49,42 @@ class ChestXRayDataLoader(keras.utils.Sequence):
 
         batch = self.__data_generation(indexes)
 
-        return (batch, batch)
+        return batch, batch
 
     def on_epoch_end(self):
         """
         This function updates the indexes at the beginning as well as at the end of each epoch
         :return: None
         """
-        self.indexes = np.arange(self.total_batch.shape[0])
+        self.indexes = np.arange(len(self.files))
         if self.shuffle:
             np.random.shuffle(self.indexes)
+
+    def __sample_multinomial(self,
+                           patch_shape: Tuple[int, int]) -> Tuple[np.ndarray, np.ndarray]:
+
+        rows, columns = patch_shape
+        mid_ix_cols = (columns-1)/2
+        mid_ix_rows = (rows-1)/2
+        var_rows = np.sqrt(rows) / 2
+        var_cols = np.sqrt(columns) / 2
+        row_ix = np.random.normal(loc=mid_ix_rows, scale=var_rows, size=self.n_patches_per_file).astype(int)
+        col_ix = np.random.normal(loc=mid_ix_cols, scale=var_cols, size=self.n_patches_per_file).astype(int)
+
+        row_ix[row_ix < 0] = 0
+        row_ix[row_ix >= rows] = rows - 1
+        col_ix[col_ix < 0] = 0
+        col_ix[col_ix >= columns] = columns -1
+
+        return row_ix, col_ix
 
     def __sliding_window_patches(self,
                                  image: np.ndarray) -> np.ndarray:
 
-        with tf.compat.v1.Session() as sess:
             image = tf.reshape(image, (1, *image.shape, 1))
 
-            patches = tf.extract_image_patches(image,
-                                               ksizes=[1, self.window_size[0], self.window_size[1], 1],
+            patches = tf.image.extract_patches(image,
+                                               sizes=[1, self.window_size[0], self.window_size[1], 1],
                                                strides=[1, self.window_size[0], self.window_size[1], 1],
                                                rates=[1, 1, 1, 1],
                                                padding='SAME')
@@ -81,22 +92,39 @@ class ChestXRayDataLoader(keras.utils.Sequence):
             new_batch_size = batch_size * num_windows_width * num_windows_height
             patches = tf.reshape(patches, (new_batch_size, *self.window_size, self.n_channels))
 
-            return sess.run(patches)
+            return patches
 
     def __data_generation(self,
-                          indexes: np.ndarray) -> np.ndarray:
+                          indexes: np.ndarray) -> tf.Tensor:
         """
         This function produces the batches of data
-        :param list_IDs_temp: The list of IDs that should be used for the batch
+        :param indexes: The list of IDs that should be used for the batch
         :return: The new batch, with shape: (batch_size, window_height, window_width, n_channels)
         """
+        tensor_list = []
+        for i in range(self.batch_size):
+            img = cv2.imread(str(self.files[indexes[i]]), cv2.IMREAD_GRAYSCALE)
+            patches_shape = np.ceil(img.shape[0]/self.window_size[0]), np.ceil(img.shape[1]/self.window_size[1])
+            row_ix, col_ix = self.__sample_multinomial(patches_shape)
+            indices = tf.constant(list(np.array(row_ix*patches_shape[1] + col_ix, dtype=np.int32)))
+            patches = self.__sliding_window_patches(np.array(img))
+            selected_patches = tf.gather(patches, indices)
+            tensor_list.append(selected_patches)
 
-        batch = self.total_batch[indexes,].copy()
+        batch = tf.cast(tf.concat(tensor_list, axis=0), tf.float32)
+        return tf.divide(batch, 255.0)
 
-        return batch
+    def get_classes(self) -> np.ndarray:
+        """
+        This function returns a numpy array, where 0s respond to healthy files and 1s to pneumonia cases.
+        This can be used when plotting a t-SNE plot for example.
+        :return: Array with length len(self.files), where True means a file / index is a healthy patient
+        """
+        return np.repeat(self.targets[self.indexes], self.n_patches_per_file)
 
 # Example usage
 """
+from pathlib import Path
 from dataloader import ChestXRayDataLoader
 chest_path = Path('./example_images')
 
