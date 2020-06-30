1b4b59c66e35b7b7dfc2340fa4c4cd254ef42821
Niels <nielsdebruin@gmail.com>
2020-06-30 20:17:39 +0200

    add dataloader v0

--- dataloader.py
+++ dataloader.py
@@ -269,4 +269,113 @@ class ChestXRayDataLoaderV1(keras.utils.Sequence):
         """
         return np.repeat(self.targets[self.indexes], self.n_patches_per_file)
 
+class ChestXRayDataLoaderV0(keras.utils.Sequence):
+    """
+    This is a custom Keras dataloader for the pneumonia Chest X-ray dataset
+    Based on: https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+    """
 
+    def __init__(self,
+                 p: Path,
+                 window_size: Tuple[int, int],
+                 n_channels: int,
+                 batch_size: int,
+                 n_patches_per_file: int,
+                 shuffle: bool = True,
+                 extension: str = '.jpeg'):
+
+        self.path: Path = p
+        self.window_size: Tuple[int, int] = window_size
+        self.n_channels: int = n_channels
+        self.batch_size: int = batch_size
+        self.n_patches_per_file: int = n_patches_per_file
+        self.shuffle: bool = shuffle
+        self.extension: str = extension
+        self.indexes: np.ndarray = np.array([])
+        self.samples = []
+
+        self.file_gen_healthy = self.path.glob(f'NORMAL/*{self.extension}')
+        healthy_files = [f for f in self.file_gen_healthy if f.is_file()]
+        print(f'{len(healthy_files)} images found in {self.path / "NORMAL"}')
+        self.file_gen_pneumonia = self.path.glob(f'PNEUMONIA/*{self.extension}')
+        pneumonia_files = [f for f in self.file_gen_pneumonia if f.is_file()]
+        print(f'{len(pneumonia_files)} images found in {self.path / "PNEUMONIA"}')
+        self.files = healthy_files + pneumonia_files
+        # print(self.files)
+        self.targets = np.array([0 if f in healthy_files else 1 for f in self.files])
+        self.on_epoch_end()
+
+    def __len__(self) -> int:
+        return int(np.floor(len(self.files) / self.batch_size))
+
+    def __getitem__(self, item) -> Tuple[np.ndarray, np.ndarray]:
+
+        indexes = self.indexes[item * self.batch_size:(item + 1) * self.batch_size]
+
+        batch = self.__data_generation(indexes)
+
+        return batch, batch
+
+    def on_epoch_end(self):
+        """
+        This function updates the indexes at the beginning as well as at the end of each epoch
+        :return: None
+        """
+        self.indexes = np.arange(len(self.files))
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def __sliding_window_patches(self,
+                                 image: np.ndarray) -> np.ndarray:
+
+            patches = tf.image.extract_patches(image,
+                                               sizes=[1, self.window_size[0], self.window_size[1], 1],
+                                               strides=[1, self.window_size[0], self.window_size[1], 1],
+                                               rates=[1, 1, 1, 1],
+                                               padding='SAME')
+            batch_size, num_windows_height, num_windows_width = patches.shape[0:3]
+            new_batch_size = batch_size * num_windows_width * num_windows_height
+            patches = tf.reshape(patches, (new_batch_size, *self.window_size, self.n_channels))
+
+            return patches
+
+    def __process_path(self, path):
+      """
+      This function will preprocess a file img path.
+      """
+      img = tf.io.read_file(path)
+
+      img = tf.io.decode_jpeg(img, channels=1, ratio=2, name="resizeand reshape")
+      
+      img = tf.expand_dims(img, 0)
+      return img
+
+    def __data_generation(self,
+                          indexes: np.ndarray) -> tf.Tensor:
+        """
+        This function produces the batches of data
+        :param indexes: The list of IDs that should be used for the batch
+        :return: The new batch, with shape: (batch_size, window_height, window_width, n_channels)
+        """
+        tensor_list = []
+        for i in range(self.batch_size):
+            img = self.__process_path(str(self.files[indexes[i]]))
+
+            patches = self.__sliding_window_patches(np.asarray(img))
+
+            indices = tf.random.shuffle(range(0, patches.shape[0]))[0:self.n_patches_per_file]
+            
+            selected_patches = tf.gather(patches, indices)
+            
+            tensor_list.append(selected_patches)
+
+        batch = tf.cast(tf.concat(tensor_list, axis=0), tf.float32)
+        return tf.divide(batch, 255.0)
+
+    def get_classes(self) -> np.ndarray:
+        """
+        This function returns a numpy array, where 0s respond to healthy files and 1s to pneumonia cases.
+        This can be used when plotting a t-SNE plot for example.
+        :return: Array with length len(self.files), where True means a file / index is a healthy patient
+        """
+        return np.repeat(self.targets[self.indexes], self.n_patches_per_file)
